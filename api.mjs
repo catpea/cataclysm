1199129cde6e6a639430361337ee41929a46bd29
Cat Pea <corporate.miou@gmail.com>
2020-08-25 12:38:54 -0400

    New Release

--- api.mjs
+++ api.mjs
@@ -5,57 +5,94 @@ import pretty from 'pretty';
 import Handlebars from 'handlebars';
 import cloneDeep from 'lodash/cloneDeep.js';
 
+import camelCase from 'lodash/camelCase.js';
+import fsWalk from '@nodelib/fs.walk';
+
+import cheerio from 'cheerio'
+import posthtml from 'posthtml'
+import posthtmlCustomElements from 'posthtml-custom-elements'
+import posthtmlNoopener from 'posthtml-noopener';
+
 export default main;
 
 async function main(setup){
 
-  console.log(setup);
-
-  console.log(`Loading template helpers`);
-  for(let item of fs.readdirSync(setup.locations.templateHelpers, { withFileTypes: true }).filter(o => o.isFile()).map(o=>o.name).filter(name=>name.endsWith('.mjs')).map(file=>({file, name: path.basename(file, '.mjs'), path: path.join(setup.locations.templateHelpers, file) }))){
-    console.log(item.path);
-    const imported = await import(item.path);
-    await imported.default({Handlebars});
+  if(fs.pathExistsSync(setup.locations.templateHelpers)){
+    for(let item of fs.readdirSync(setup.locations.templateHelpers, { withFileTypes: true }).filter(o => o.isFile()).map(o=>o.name).filter(name=>name.endsWith('.mjs')).map(file=>({file, name: path.basename(file, '.mjs'), path: path.join(setup.locations.templateHelpers, file) }))){
+      const imported = await import(item.path);
+      await imported.default({Handlebars});
+    }
   }
 
-  console.log(`Loading template specific helpers`);
-  for(let item of fs.readdirSync(setup.locations.templateRootHelpers, { withFileTypes: true }).filter(o => o.isFile()).map(o=>o.name).filter(name=>name.endsWith('.mjs')).map(file=>({file, name: path.basename(file, '.mjs'), path: path.join(setup.locations.templateRootHelpers, file) }))){
-    console.log(item.path);
-    const imported = await import(item.path);
-    await imported.default({Handlebars});
+  if(fs.pathExistsSync(setup.locations.templateRootHelpers)){
+    for(let item of fs.readdirSync(setup.locations.templateRootHelpers, { withFileTypes: true }).filter(o => o.isFile()).map(o=>o.name).filter(name=>name.endsWith('.mjs')).map(file=>({file, name: path.basename(file, '.mjs'), path: path.join(setup.locations.templateRootHelpers, file) }))){
+      const imported = await import(item.path);
+      await imported.default({Handlebars});
+    }
   }
 
-  console.log(`Loading template specific partials`);
-  for(let item of fs.readdirSync(setup.locations.templateRootPartials, { withFileTypes: true }).filter(o => o.isFile()).map(o=>o.name).filter(name=>name.endsWith('.hbs')).map(file=>({file, name: path.basename(file, '.hbs'), path: path.join(setup.locations.templateRootPartials, file) }))){
-    console.log(item.path);
-    Handlebars.registerPartial(item.name, fs.readFileSync(item.path).toString())
+  if(fs.pathExistsSync(setup.locations.templateRootPartials)){
+    for(let item of fsWalk.walkSync(setup.locations.templateRootPartials).filter(o=>o.name.endsWith('.hbs')).map(({name:file,path:fullPath})=>({file, name: camelCase(path.join(path.dirname(path.relative(setup.locations.templateRootPartials, fullPath)), path.basename(file, '.hbs'))), path:fullPath }))){
+      // console.log(item.path);
+      Handlebars.registerPartial(item.name, fs.readFileSync(item.path).toString())
+    }
   }
 
   // Page Creation
-  console.log(`Creating pages`);
+  // console.log(`Creating pages`);
   for(let item of fs.readdirSync(setup.locations.templateRootPages, { withFileTypes: true }).filter(o => o.isFile()).map(o=>o.name).filter(name=>name.endsWith('.hbs')).map(file=>({file, name: path.basename(file, '.hbs'), path: path.join(setup.locations.templateRootPages, file) }))){
     const destination = path.join(setup.locations.destination, item.name + '.html')
-    const template = Handlebars.compile( fs.readFileSync(item.path).toString() );
-    let html = template(Object.assign({id:item.name}, cloneDeep(setup)));
-    let code = pretty(html, {ocd: true});
+    const file = item.path;
+    // const data = Object.assign({pageId:item.name}, cloneDeep(setup));
+    const page = {
+      id: item.name,
+      file: item.name + '.html',
+    };
+    const data = Object.assign({page}, cloneDeep(setup));
+
+    let code = template({file, data});
+
     fs.writeFileSync(destination, code);
-    console.log(`Created ${destination}`);
+    // console.log(`Created ${destination}`);
   }
 
+  if(fs.pathExistsSync(setup.locations.templateRootPlugins)){
   // Plugin Processing
-  console.log(`Executing plugins`);
+  // console.log(`Executing plugins`);
   for(let plugin of fs.readdirSync(setup.locations.templateRootPlugins, { withFileTypes: true }).filter(o => o.isDirectory()).map(o=>o.name).map(name=>({name, path: path.join(setup.locations.templateRootPlugins, name), module: path.join(setup.locations.templateRootPlugins, name, 'index.mjs') }))){
     const imported = await import(plugin.module);
-    await imported.default({plugin, setup:Object.assign({id:plugin.name}, cloneDeep(setup)), Handlebars});
+
+    const page = {
+      id: plugin.name,
+    };
+    const data = Object.assign({page}, cloneDeep(setup));
+
+    await imported.default({id:plugin.name, plugin, setup:data, template});
+  }
+  }
+
+  // FINISH UP
+  // console.log('Copying Website Files');
+  if(fs.pathExistsSync(setup.locations.websiteFiles)) fs.copySync(setup.locations.websiteFiles, setup.locations.destination)
+  // console.log('Copying Template Files');
+  if(fs.pathExistsSync(setup.locations.templateFiles)) fs.copySync(setup.locations.templateFiles, setup.locations.destination)
+
   }
 
 
+function smarter(html){
+  const $ = cheerio.load(html);
+  // $('img').addClass('img-fluid rounded shadow');
+  return $.html();
+}
 
-
-  // FINISH UP
-  console.log('Copying Website Files');
-  fs.copySync(setup.locations.websiteFiles, setup.locations.destination)
-  console.log('Copying Template Files');
-  fs.copySync(setup.locations.templateFiles, setup.locations.destination)
-
+function template({file, data}){
+  return [file]
+    .map(input=>fs.readFileSync(input).toString())
+    .map(input=>Handlebars.compile(input))
+    .map(input=>input(data))
+    .map(input=>posthtml().use(posthtmlCustomElements(), posthtmlNoopener.noopener()).process(input,{sync:true}).html)
+    .map(input=>smarter(input))
+    .map(input=>pretty(input,{ocd: true}))
+    .pop();
 }
--- cli.mjs
+++ cli.mjs
@@ -1,36 +1,60 @@
 #!/usr/bin/env -S node --experimental-modules --trace-warnings
 
 import path from 'path';
+import yargs from 'yargs';
+import kebab from 'lodash/kebabCase.js';
 import api from './api.mjs';
 
 async function main(){
+
   const defaults = {
     destination: 'docs',
     template: 'main',
-    locations: {}
   };
-
-
 
   const options = await (await import(path.join(process.cwd(), 'cataclysm.mjs'))).default();
   const data = await (await import(path.join(process.cwd(), 'src/data/index.mjs'))).default();
-  const setup = Object.assign({}, data, defaults, options);
 
-  setup.locations.destination = path.join(process.cwd(), setup.destination);
+  const setup = Object.assign({
+    locations: {},
+    options: Object.assign({}, defaults, options),
+    data: data,
+    content: null,
+  });
 
-  setup.locations.templateHelpers = path.join(process.cwd(), 'src/templates/helpers');
-  setup.locations.templateRoot = path.join(process.cwd(), path.join('src/templates/', setup.template));
+  setup.locations.root = path.join(process.cwd());
+  setup.locations.options = path.join(setup.locations.root, 'cataclysm.mjs');
+  setup.locations.dataRoot = path.join(setup.locations.root, 'src', 'data');
+  setup.locations.dataModule = path.join(setup.locations.dataRoot, 'index.mjs');
+
+  setup.locations.destination = path.join(setup.locations.root, setup.options.destination);
+  setup.locations.templateHelpers = path.join(setup.locations.root, 'src/templates/helpers');
+  setup.locations.templateRoot = path.join(setup.locations.root, path.join('src/templates/', setup.options.template));
   setup.locations.templateRootHelpers = path.join(setup.locations.templateRoot, 'helpers');
   setup.locations.templateRootPartials = path.join(setup.locations.templateRoot, 'partials');
   setup.locations.templateRootPages = path.join(setup.locations.templateRoot, 'pages');
   setup.locations.templateRootPlugins = path.join(setup.locations.templateRoot, 'plugins');
-
-  setup.locations.websiteFiles = path.join(process.cwd(), 'src/files');
+  setup.locations.websiteFiles = path.join(setup.locations.root, 'src/files');
   setup.locations.templateFiles = path.join(setup.locations.templateRoot, 'files');
 
+  const locations = yargs.argv.locations||yargs.argv.l;
+  if(locations){
+      if(typeof locations === 'boolean'){
+        if(locations === true){
+          Object.entries(setup.locations)
+          .map(([k,v])=>`${kebab(k)} ${v}`)
+          .map(str=>console.log(str))
+        }
+      }else if(typeof locations === 'string'){
+        Object.entries(setup.locations)
+        .filter(([k,v])=>(kebab(k)==locations))
+        .map(([k,v])=>v)
+        .map(str=>console.log(str))
+      }
+  }else{
+    await api(setup);
+  }
 
-
-  await api(setup);
 }
 
 main();
