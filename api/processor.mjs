72f3a9b05536ca7ece0213b6aab8e62505bc8e23
catpea <corporate.miou@gmail.com>
2020-09-02 18:04:42 -0400

    Move string interpolation into core traversal.

--- api/processor.mjs
+++ api/processor.mjs
@@ -12,7 +12,7 @@ import fsWalk from '@nodelib/fs.walk';
 export default main;
 
 function load(html){
-  const $ = cheerio.load(html, {normalizeWhitespace: false, xmlMode: true});
+  const $ = cheerio.load(html);
   return $;
 }
 
@@ -20,15 +20,14 @@ async function main(setup, files){
 
   const db = database(setup, files);
 
-  //console.log(db.partials);
+
 
   for(let page of db.pages){
     const $ = load(page.content);
 
     const instance = {
       template: db.partials.reduce((root,item)=>{ root[item.name] = item.content; return root; }, {}), // the template database is seeded with partials, extended within each page by <template/>s.
-      context:
-      setup,
+      context: Object.assign({}, setup, {page:{ id: page.name, file: page.name + '.html' }} ),
       db,
       $
     };
@@ -36,51 +35,125 @@ async function main(setup, files){
     // This should happen once, because templates are stripped off asap, to be used later on.
     templator(instance);
     traverse($('html'), instance);
-    page.html = $.html();
-    //page.html = pretty(page.html)
-    //page.html = interpolate(page.html, setup); // final interpolation pass, at this point all the newly created stuff had things interpolated by their creators.
-    //console.log(page.html);
 
+
+    page.html = $.html();
+    page.html = pretty(page.html)
     save(setup, page)
 
   }
-  //console.log(`HTML (${db.pages.length}): ${db.pages.map(i=>`${i.name} (${humanize.filesize(i.html.length)})`)}`);
+
 }
 
 function traverse(root, {template, context, page, db, $}){
   // $(root).addClass('traversed');
   $(root)
   .contents() // Gets the branches of each branch in the set of matched branchs, including text and comment nodes.
-  .filter((index, branch) => branch.nodeType == 1) // select standard nodes only
+  //.filter((index, branch) => branch.nodeType == 1) // select standard nodes only
   .each(function (index, branch) {
+
+
+    if( branch.nodeType === 3 ){
+      const text = $.html(branch);
+      $(branch).replaceWith( interpolate(text, context) );
+    }
+
+    const state = {};
+    state.execute = 'nominal';
+
     if($(branch).attr('if')){
-      const path = $(branch).attr('if');
+      state.execute = 'if';
+      state.path = $(branch).attr('if');
+
+      state.is = $(branch).attr('is');
+      state.not = $(branch).attr('not');
+
+      $(branch).attr('if', null);
+      $(branch).attr('is', null);
+      $(branch).attr('not', null);
+      state.html = $.html(branch) + '\n'; // everything including parent because this is an attribute
+
+    } else if(branch.name == 'if'){
+      state.execute = 'if';
+      state.path = $(branch).attr('path');
+
+      state.is = $(branch).attr('is');
+      state.not = $(branch).attr('not');
+
+      $(branch).attr('path', null);
+      $(branch).attr('is', null);
+      $(branch).attr('not', null);
+      state.html = $(branch).html() + '\n'; // contents only because this is a node we want to get rid of.
+
+    } else if($(branch).attr('each')){
+      state.execute = 'each';
+      state.path = $(branch).attr('each');
+      $(branch).attr('each', null);
+      state.html = $.html(branch) + '\n'; // everything including parent because this is an attribute
+      state.removeBranch = function(){ $(branch).remove(); }
+
+    } else if(branch.name == 'each'){
+      state.execute = 'each';
+      state.path = $(branch).attr('path');
+      state.html = $(branch).html() + '\n'; // contents only because this is a node we want to get rid of.
+      //console.log('found each', Object.keys(context).join(", "), state.html);
+      $(branch).attr('path', null);
+      state.removeBranch = function(){ $(branch).remove(); }
+
+    }else if( Object.keys(template).includes(branch.name) ){
+      state.execute = 'template';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if(state.execute == 'if'){
+      const path = state.path;
+      const html = state.html;
       const dereferenced = dereference(context, path);
-      $(branch).attr('if', null); // remove the spent attribute
-      $(branch).attr('if-result', !!dereferenced); // remove the spent attribute
-      $(branch).before(`<!-- ${address($, branch)}: if path "${path}" returned "${dereferenced}" (keys were ${Object.keys(context).join(", ")}) -->`);
-      if(dereferenced === undefined){
+      //$(branch).before(`<!-- IF: address://${address($, branch)} -->`);
+      //console.log( context.id );
+      let outcome = undefined;
+
+      if(state.is !== undefined){
+        outcome = ( dereferenced == state.is)
+      } else if (state.not !== undefined) {
+        outcome = ( dereferenced != state.not)
+      } else {
+        outcome = !!dereferenced;
+      }
+
+      $(branch).before(`<!-- if path "${path}" is:(${state.is}) not:(${state.not}) resulted in outcome=(${outcome}) returned "${dereferenced}" (keys were ${Object.keys(context).join(", ")}) -->`);
+      if(outcome){
+
+        // traverse prior to interpolation
+        // const html = $.html(branch) + '\n'; // get string for interpolation
+        //const interpolated = html;
+        // //const interpolated = interpolate(html, context);
+        const $new = load(html);
+        traverse($new.root(), {template, context, page, db, $:$new});
+        $(branch).replaceWith($new.html());
+      }else{
+        // NOTE: branch remove for <if> and <div if=""/> is functionally the same!
         $(branch).remove(); // remove the entire node as it failed the logic test
         // nothing to traverse, the node is gone now
-      }else{
-        // traverse prior to interpolation
-        traverse(branch, {template, context, page, db, $});
-        const html = $.html(branch) + '\n'; // get string for interpolation
-        const interpolated = interpolate(html, context);
-        const $new = load(interpolated);
-        $(branch).replaceWith($new.html());
       }
-    }else if($(branch).attr('each')){
 
-      const path = $(branch).attr('each');
-
-      $(branch).attr('each', null);
-
-      const html = $.html(branch) + '\n';
-
+    } else if(state.execute == 'each'){
+      const path = state.path;
+      const html = state.html;
       let dereferenced = dereference(context, path);
-
-
       // SLICE
       const slice = decodeSlice($(branch).attr('slice'));
       $(branch).attr('slice', null);
@@ -89,7 +162,8 @@ function traverse(root, {template, context, page, db, $}){
           dereferenced = dereferenced.slice(...slice);
         }
       }
-
+      //$(branch).before(`<!-- EACH: address://${address($, branch)} -->`);
+      $(branch).before(`<!-- EACH path "${path}" returned "${dereferenced}" (keys were ${Object.keys(context).join(", ")}) -->`);
       // ITERATION
       let index = 0;
       if(dereferenced){
@@ -98,39 +172,45 @@ function traverse(root, {template, context, page, db, $}){
           const interpolated = interpolate(html, newContext);
           const $new = load(interpolated);
           traverse($new.root(), {template, context:newContext, page, db, $:$new});
+
           $(branch).before($new.html());
           index++;
         }
       }
+      state.removeBranch()
 
-      $(branch).remove();
-    }else if( Object.keys(template).includes(branch.name) ){
+    } else if(state.execute == 'template'){
       const name = branch.name;
       const html = template[name];
       const content = $.html(branch);
-      const interpolated = interpolate(html, context);
+      const interpolated = html;
+      //TODO: when to do interpolation here?
+      //const interpolated = interpolate(html, context);
       const $new = load(interpolated);
       $(branch).before(`<!-- template ${name} is mounted here here (keys were ${Object.keys(context).join(", ")}) -->`);
 
+      // TODO: check slot names in immediate nodes only
       $new('slot[name]').each(function (index, templateSlot) {
         const slotName = $(templateSlot).attr('name');
         const $content = load(content);
-
-        // comes from the body
+        // comes from the body of the call to a template
         const selection = $content(`*[slot='${slotName}']`).html();
         const existingContent = $(templateSlot).html();
-
         $(selection).attr('slot', null)
         $new(templateSlot).replaceWith( selection||existingContent )
       });
-
       traverse($new.root(), {template, context, page, db, $:$new}); // once slots have been done traverse the shadow root
+
+
+      // ADD INTERPOLATION HERE!?
       $(branch).replaceWith($new.html()); // and add the shadowroot back
+      interpolateHtml($, branch, context)
 
     }else{
       traverse(branch, {template, context, page, db, $});
     }
-  })
+
+  }) // cheerio each
 }
 
 function database(setup, files){
@@ -190,7 +270,7 @@ function address($, root){
   .parents()
   .map(function(i, el) {
     return id(this);
-  }).get().reverse().concat(id(root)).join(' -> ');
+  }).get().reverse().concat(id(root)).join('/');
   return response;
 }
 
@@ -199,10 +279,13 @@ function interpolate(t, c){
 }
 
 function interpolateHtml($, node, context){
+
+
+
   const html = $.html(node);
   const interpolated = interpolate(html, context);
-
   const $new = load(interpolated);
+  //console.log($new.html());
   $(node).replaceWith($new.html());
 
 }
@@ -217,7 +300,11 @@ function interpolateHtml($, node, context){
 
 function save(setup, page){
     const destination = path.join(setup.locations.destination, page.name + '2' + '.html')
-    fs.writeFileSync(destination, page.html);
+
+    let html = page.html;
+    //html = cheerio.load(html).html();
+    fs.writeFileSync(destination, html);
+
     console.log(`INFO: saved ${destination} (${humanize.filesize(page.html.length)})`);
 }
 
