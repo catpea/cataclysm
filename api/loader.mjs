6015b025450debe8a9f410803967fbee6f5d28af
catpea <corporate.miou@gmail.com>
2020-09-02 14:17:33 -0400

    Base functionality looking great!

--- api/loader.mjs
+++ api/loader.mjs
@@ -2,6 +2,7 @@ import {inspect} from 'util';
 import fs from 'fs-extra';
 import path from 'path';
 import camelCase from 'lodash/camelCase.js';
+import kebabCase from 'lodash/kebabCase.js';
 import fsWalk from '@nodelib/fs.walk';
 
 export default main;
@@ -10,7 +11,7 @@ async function main(setup){
   const files = []
   .concat(shallow('helper', setup.locations.templateHelpers, '.mjs'))
   .concat(shallow('helper', setup.locations.templateRootHelpers, '.mjs'))
-  .concat(shallow('partial', setup.locations.templateRootPartials, '.hbs'))
+  .concat(intense('partial', setup.locations.templateRootPartials, '.html'))
   .concat(shallow('plugin', setup.locations.templateRootPlugins, '.mjs'))
   .concat(intense('page', setup.locations.templateRootPages, '.html'))
   return files;
@@ -21,11 +22,11 @@ function intense(type, location, extension){
   if(fs.pathExistsSync(location)){
     const items = fsWalk.walkSync(location)
     .filter(o=>o.name.endsWith(extension))
-    .map(({name:filename, path:location})=>({
+    .map(({name:filename, path:base})=>({
       filename,
       type,
-      name: camelCase(path.join(path.dirname(path.relative(location, location)), path.basename(filename, extension))),
-      location
+      name: kebabCase(path.join(path.dirname(path.relative(location, base)), path.basename(filename, extension))),
+      location:base
     }))
     .forEach(item=>response.push(item))
   }
--- api/processor.mjs
+++ api/processor.mjs
@@ -11,20 +11,40 @@ import fsWalk from '@nodelib/fs.walk';
 
 export default main;
 
+function load(html){
+  const $ = cheerio.load(html, {normalizeWhitespace: false, xmlMode: true});
+  return $;
+}
+
 async function main(setup, files){
 
   const db = database(setup, files);
+
+  //console.log(db.partials);
+
   for(let page of db.pages){
-    const $ = cheerio.load(page.content);
-    const instance = {template:{}, context:setup, db, $};
+    const $ = load(page.content);
+
+    const instance = {
+      template: db.partials.reduce((root,item)=>{ root[item.name] = item.content; return root; }, {}), // the template database is seeded with partials, extended within each page by <template/>s.
+      context:
+      setup,
+      db,
+      $
+    };
+
     // This should happen once, because templates are stripped off asap, to be used later on.
     templator(instance);
     traverse($('html'), instance);
-    page.html = pretty($.html());
-    // page.html = interpolate(page.html, setup); // final interpolation pass, at this point all the newly created stuff had things interpolated by their creators.
-    console.log(page.html);
+    page.html = $.html();
+    //page.html = pretty(page.html)
+    //page.html = interpolate(page.html, setup); // final interpolation pass, at this point all the newly created stuff had things interpolated by their creators.
+    //console.log(page.html);
+
+    save(setup, page)
+
   }
-  console.log(`HTML (${db.pages.length}): ${db.pages.map(i=>`${i.name} (${humanize.filesize(i.html.length)})`)}`);
+  //console.log(`HTML (${db.pages.length}): ${db.pages.map(i=>`${i.name} (${humanize.filesize(i.html.length)})`)}`);
 }
 
 function traverse(root, {template, context, page, db, $}){
@@ -47,38 +67,63 @@ function traverse(root, {template, context, page, db, $}){
         traverse(branch, {template, context, page, db, $});
         const html = $.html(branch) + '\n'; // get string for interpolation
         const interpolated = interpolate(html, context);
-        const $new = cheerio.load(interpolated);
+        const $new = load(interpolated);
         $(branch).replaceWith($new.html());
       }
     }else if($(branch).attr('each')){
+
       const path = $(branch).attr('each');
+
       $(branch).attr('each', null);
+
       const html = $.html(branch) + '\n';
-      let index = 0;
-      const dereferenced = dereference(context, path);
-      for(let item of dereferenced){
-        const newContext = Object.assign({}, context, item, {index});
-        const interpolated = interpolate(html, newContext);
-        const $new = cheerio.load(interpolated);
-        traverse($new.root(), {template, context:newContext, page, db, $:$new});
-        $(branch).before($new.html());
-        index++;
+
+      let dereferenced = dereference(context, path);
+
+
+      // SLICE
+      const slice = decodeSlice($(branch).attr('slice'));
+      $(branch).attr('slice', null);
+      if(slice){
+        if(dereferenced){
+          dereferenced = dereferenced.slice(...slice);
+        }
       }
+
+      // ITERATION
+      let index = 0;
+      if(dereferenced){
+        for(let item of dereferenced){
+          const newContext = Object.assign({}, context, item, {index});
+          const interpolated = interpolate(html, newContext);
+          const $new = load(interpolated);
+          traverse($new.root(), {template, context:newContext, page, db, $:$new});
+          $(branch).before($new.html());
+          index++;
+        }
+      }
+
       $(branch).remove();
     }else if( Object.keys(template).includes(branch.name) ){
       const name = branch.name;
       const html = template[name];
       const content = $.html(branch);
       const interpolated = interpolate(html, context);
-      const $new = cheerio.load(interpolated);
-      $(branch).before(`<!-- template ${name} is mounted here here -->`);
+      const $new = load(interpolated);
+      $(branch).before(`<!-- template ${name} is mounted here here (keys were ${Object.keys(context).join(", ")}) -->`);
+
       $new('slot[name]').each(function (index, templateSlot) {
         const slotName = $(templateSlot).attr('name');
-        const $content = cheerio.load(content);
-        const selection = $content(`*[slot='${slotName}']`);
+        const $content = load(content);
+
+        // comes from the body
+        const selection = $content(`*[slot='${slotName}']`).html();
+        const existingContent = $(templateSlot).html();
+
         $(selection).attr('slot', null)
-        $new(templateSlot).replaceWith( selection )
+        $new(templateSlot).replaceWith( selection||existingContent )
       });
+
       traverse($new.root(), {template, context, page, db, $:$new}); // once slots have been done traverse the shadow root
       $(branch).replaceWith($new.html()); // and add the shadowroot back
 
@@ -102,6 +147,8 @@ function database(setup, files){
   db.helpers.reduce((root,item)=>{ if(!root.helper) root.helper = {}; root.helper[item.name] = item; return root; }, db);
   db.plugins.reduce((root,item)=>{ if(!root.plugin) root.plugin = {}; root.plugin[item.name] = item; return root; }, db);
   db.pages.reduce((root,item)=>{ if(!root.page) root.page = {}; root.page[item.name] = item; return root; }, db);
+
+
 
   return db;
 }
@@ -155,7 +202,40 @@ function interpolateHtml($, node, context){
   const html = $.html(node);
   const interpolated = interpolate(html, context);
 
-  const $new = cheerio.load(interpolated);
+  const $new = load(interpolated);
   $(node).replaceWith($new.html());
 
+}
+
+
+
+
+
+
+
+
+
+function save(setup, page){
+    const destination = path.join(setup.locations.destination, page.name + '2' + '.html')
+    fs.writeFileSync(destination, page.html);
+    console.log(`INFO: saved ${destination} (${humanize.filesize(page.html.length)})`);
+}
+
+
+
+
+
+
+
+
+function decodeSlice(str){
+  let response = null;
+
+  if(str){
+    response = str.split(',')
+    .map(i=>i.trim())
+    .map(i=>parseInt(i))
+  }
+
+  return response;
 }
